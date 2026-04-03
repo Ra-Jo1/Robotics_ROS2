17fa7fa87fc6cdc18de9b0d975453dc7f030de21
Jo <jonatra1@gmail.com>
2026-04-03 20:18:33 +0200

    Implement keyboard control for turtle movement

---
+++ Prog_1_turtle_controller/keyboard_control.py
@@ -0,0 +1,45 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+import sys, termios, tty
+
+class KeyboardController(Node):
+
+    def __init__(self):
+        super().__init__('keyboard_controller')
+        self.publisher = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+
+    def get_key(self):
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        tty.setraw(fd)
+        key = sys.stdin.read(1)
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return key
+
+    def run(self):
+        print("Contrôle : ZQSD (ou WASD), X pour quitter")
+
+        while True:
+            key = self.get_key()
+            msg = Twist()
+
+            if key == 'z':  # avant
+                msg.linear.x = 2.0
+            elif key == 's':  # arrière
+                msg.linear.x = -2.0
+            elif key == 'q':  # gauche
+                msg.angular.z = 2.0
+            elif key == 'd':  # droite
+                msg.angular.z = -2.0
+            elif key == 'x':
+                break
+
+            self.publisher.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = KeyboardController()
+    node.run()
+    node.destroy_node()
+    rclpy.shutdown()
